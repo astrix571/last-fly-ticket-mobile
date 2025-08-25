2b92315d3ca97640dffeeab7c3d093a3baf04618
projecto46 <projecto46@protonmail.com>
2025-08-25 10:19:15 +0300

    כןס

--- i18n/index.ts
+++ i18n/index.ts
@@ -1,19 +1,14 @@
-import { getLocales } from 'expo-localization';
+import * as Localization from 'expo-localization';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+
 import en from './locales/en.json';
 import he from './locales/he.json';
 
-const resources = {
-  en: { translation: en },
-  he: { translation: he },
-};
-
 i18n.use(initReactI18next).init({
-  compatibilityJSON: 'v3',
-  resources,
+  resources: { en: { translation: en }, he: { translation: he } },
   fallbackLng: 'en',
-  lng: getLocales()[0].languageCode,
+  lng: Localization.getLocales()[0].languageCode,
   interpolation: { escapeValue: false },
 });
 
