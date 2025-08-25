650fc7708ad7c5de764410704190f605aec7a522
projecto46 <projecto46@protonmail.com>
2025-08-25 15:19:23 +0300

    90

--- components/HapticTab.tsx
+++ components/HapticTab.tsx
@@ -1,3 +1,5 @@
+// components/HapticTab.tsx
+
 import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
 import { PlatformPressable } from '@react-navigation/elements';
 import * as Haptics from 'expo-haptics';
