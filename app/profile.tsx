45a15e8eaedc4faefe5c85c7df6a2d0fcdce52c7
projecto46 <projecto46@protonmail.com>
2025-08-28 09:17:54 +0300

    10

---
+++ app/profile.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function ProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>This is the Profile Tab</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 22,
+  },
+});
