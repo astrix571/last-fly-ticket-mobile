0548abc62e5ce8f42b26df2770a2ad0a55a23e01
projecto46 <projecto46@protonmail.com>
2025-08-28 20:20:16 +0300

    2

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,9 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          extensions: ['.js', '.jsx', '.ts', '.tsx'],
-          alias: {
-            '@components': './components',
-            '@hooks': './hooks',
-          },
-        },
-      ],
-      [
-        'dotenv-import',
-        {
-          moduleName: '@env',
-          path: '.env',
-          safe: false,
-          allowUndefined: true,
-        },
-      ],
-    ],
-  };
-};
+[
+  'dotenv-import',
+  {
+    moduleName: '@env',
+    path: '.env',
+    safe: false,
+    allowUndefined: true,
+  },
+],
