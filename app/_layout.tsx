4876d0abe10e58a391554ec0c928ef3ddd2dc743
projecto46 <projecto46@protonmail.com>
2025-08-28 14:23:20 +0300

    6

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -23,7 +23,10 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+      <Stack initialRouteName="index">
+        <Stack.Screen name="index" options={{ title: 'Mood Selector' }} />
+        <Stack.Screen name="destinations" />
+        <Stack.Screen name="flights" />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
