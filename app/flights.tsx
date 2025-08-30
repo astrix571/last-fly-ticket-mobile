ca0de002dd6c3e884ac58a4fb2d1ced2f02f588a
projecto46 <projecto46@protonmail.com>
2025-08-31 00:51:02 +0300

    1

--- app/flights.tsx
+++ app/flights.tsx
@@ -1,37 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { ActivityIndicator, FlatList, Text, View } from "react-native";
-
+import { Flight, getFlights } from "../services/flights";
 
 export default function FlightsScreen() {
   const [flights, setFlights] = useState<Flight[]>([]);
   const [loading, setLoading] = useState(true);
 
   const departure_id = "TLV";
-
   const destinations = ["ATH", "BCN", "FCO", "CDG", "VCE", "JMK", "NCE", "SPU"];
   const arrival_id = destinations[Math.floor(Math.random() * destinations.length)];
 
   useEffect(() => {
     const fetchFlights = async () => {
-      const res = await searchFlights({
-        departure_id,
-        arrival_id,
-        currency: "USD",
-        country_code: "US",
-        language_code: "en-US",
-        travel_class: "ECONOMY",
-        show_hidden: "false",
-        search_type: "best",
-        adults: "1",
-      });
-
-      console.log("🔍 RAW API RESPONSE:", res);
+      const res = await getFlights(arrival_id);
       setFlights(res || []);
       setLoading(false);
     };
 
     fetchFlights();
-  }, []);
+  }, [arrival_id]);
 
   if (loading) {
     return (
